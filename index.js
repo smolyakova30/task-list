192ed49122a5711109783b68af137a1e65653645
Olga <o.smolyakova30@gmail.com>
2022-08-28 12:40:06 -0500

    added final touch for @madia

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const inputFiled = document.querySelector('.inputFiled');
 const btn = document.querySelector('.btn');
 const toDoContainer = document.querySelector('.toDoContainer');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () => {
     const item = document.createElement("li");
+    console.log(btn)
     item.innerText = inputFiled.value;
+    console.log(item)
+    item.classList.add('todoAdded')
     toDoContainer.appendChild(item);
+    inputFiled.value = '';
+
+
+
+    item.addEventListener('click', () => {
+        item.classList.add('todoComleted')
+    })
+
+
+    item.addEventListener('dblclick', () => {
+        toDoContainer.removeChild(item);
+    })
+
+
 });
+
+
 
