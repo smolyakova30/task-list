46be08d93d136a6b236b6e726d79bc24c2fd29a8
Olga <o.smolyakova30@gmail.com>
2022-08-28 11:53:06 -0500

    added function fr button, to add task

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const inputFiled = document.querySelector('.inputFiled');
 const btn = document.querySelector('.btn');
-const toDoContainer = document.querySelector('.toDoContainer')
+const toDoContainer = document.querySelector('.toDoContainer');
+
+btn.addEventListener('click', function(){
+    const item = document.createElement("li");
+    item.innerText = inputFiled.value;
+    toDoContainer.appendChild(item);
+});
+
